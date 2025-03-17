c8468a123695ff5a4a275dbc4adf3ffb7297a3ee
a1919727 <152280862+a1919727@users.noreply.github.com>
2025-03-18 09:51:22 +1030

    Proxy server connects to client and gets request

--- Proxy.py
+++ Proxy.py
@@ -55,6 +55,7 @@ while True:
   # Accept connection from client and store in the clientSocket
   try:
     # ~~~~ INSERT CODE ~~~~
+    clientSocket,addr = serverSocket.accept()
     # ~~~~ END CODE INSERT ~~~~
     print ('Received a connection')
   except:
@@ -64,6 +65,7 @@ while True:
   # Get HTTP request from client
   # and store it in the variable: message_bytes
   # ~~~~ INSERT CODE ~~~~
+  message_bytes = clientSocket.recv(1024)
   # ~~~~ END CODE INSERT ~~~~
   message = message_bytes.decode('utf-8')
   print ('Received request:')
