9288b2e3e6d5ef5891f52efdc47d8562151a3467
a1919727 <152280862+a1919727@users.noreply.github.com>
2025-03-19 09:49:27 +1030

    Test successful.  Connected to origin Server. Fix a bug.
    The port of the origin server is 80 in HTTP.

--- Proxy.py
+++ Proxy.py
@@ -139,7 +139,7 @@ while True:
       address = socket.gethostbyname(hostname)
       # Connect to the origin server
       # ~~~~ INSERT CODE ~~~~
-      originServerSocket.connect((address,proxyPort))
+      originServerSocket.connect((address,80))
       # ~~~~ END CODE INSERT ~~~~
       print ('Connected to origin Server')
 
